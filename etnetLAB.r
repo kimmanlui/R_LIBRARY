a59c3f9e56fe1dd3f7f16ffc6f53cd9e581ce923
kimmanlui <cskmlui@hotmail.com>
2026-03-10 16:59:15 +0800

    Add files via upload

---
+++ etnetLAB.r
@@ -0,0 +1,245 @@
+library(httr)
+library(RCurl)
+library(XML)
+#library(rvest)
+#library(RMySQL)
+#library(dbx)
+library(stringr)
+
+sleep_until <- function(target_time) {
+  # Get the current time in Hong Kong Time (HKT)
+  current_time_hkt <- with_tz(Sys.time(), tzone = "Asia/Hong_Kong")  # Assuming system time is in UTC
+  
+  # Convert the target time (local time) to a POSIXct object in HKT
+  target_time_today <- as.POSIXct(paste(as.Date(current_time_hkt), target_time), tz = "Asia/Hong_Kong")
+  
+  # If the target time has already passed today, set it for tomorrow
+  if (target_time_today < current_time_hkt) {
+    target_time_today <- target_time_today + days(1)
+  }
+  
+  # Calculate the sleep duration in seconds
+  sleep_duration <- as.numeric(difftime(target_time_today, current_time_hkt, units = "secs"))
+ 
+  # Sleep for the calculated duration
+  cat(paste("Sleeping for", sleep_duration, "seconds until", target_time_today, "\n"))
+  Sys.sleep(sleep_duration)
+  cat("Woke up!\n")
+}
+
+getET_ohlc=function () 
+{
+    library(rvest)
+    base_url <- "http://www.etnet.com.hk/www/eng/futures/index.php"
+    doc <- read_html(base_url)
+    futures_quote <- doc %>% html_nodes(".FuturesQuoteBox") %>% 
+        html_nodes(".FuturesQuoteContent") %>% .[[3]] %>% html_nodes(".FuturesQuoteOthers") %>% 
+        .[[1]] %>% html_text()
+    digits <- str_extract_all(futures_quote, "\\d{1,3}(?:,\\d{3})*")[[1]]
+    cleaned_text <- gsub("[^0-9,.]", " ", futures_quote)
+
+    hsi_quote  <- doc %>% html_nodes(".FuturesQuoteBox") %>% 
+        html_nodes(".FuturesQuoteContent") %>% .[[3]] %>% html_text()
+    hsi_digits <- str_extract_all(hsi_quote, "\\d{1,3}(?:,\\d{3})*")[[1]]
+    print(hsi_digits[1])
+   #  cleaned_hsi_text <- gsub("[^0-9,.]", " ", futures_quote)
+   #print(hsi_quote)
+    hsi_price = as.numeric(gsub(",", "", hsi_digits[1]))
+    digits <- unlist(strsplit(cleaned_text, " "))
+    digits <- digits[digits != ""]
+    result = as.numeric(gsub(",", "", digits))
+    return(data.frame(o = round(result[2]), h = round(result[3]), l = round(result[4]), 
+        c = hsi_price))
+}
+
+
+
+extractRecord=function(rec, db, col_name=c('Weekday', 'bd','bp'), type=c(0,1,1))
+{
+    if (nrow(rec)>1) stop('rec should have 1 row')
+    for (i in 1:length(col_name))
+    {
+        if (type[i]==1) {
+            col_name_A=paste0(col_name[i],'_1')
+            col_name_B=paste0(col_name[i],'_2')
+            col_name_A_OLD=paste0(col_name[i],'_LOWER')
+            col_name_B_OLD=paste0(col_name[i],'_UPPER')
+            #print(col_name_A)
+            #print(col_name_B)
+            #print(col_name_A_OLD)
+            #print(col_name_B_OLD)
+            #print(names(db))
+            
+            if ( col_name_A_OLD %in% names(db)) db <- db %>% rename(!!col_name_A := all_of(col_name_A_OLD))
+            if ( col_name_B_OLD %in% names(db)) db <- db %>% rename(!!col_name_B := all_of(col_name_B_OLD))
+            #print(col_name_B_OLD)
+        }
+    }
+    if ('Day' %in% names(db)) db = db %>%  rename('Weekday' ='Day'  )
+    if (!all(col_name %in% names(rec))) {  stop("Some col_name(s) not in rec")}
+    for (i in 1:length(col_name))
+    {
+        if (type[i]==1) {
+            col_name_A=paste0(col_name[i],'_1')
+            col_name_B=paste0(col_name[i],'_2')
+            
+
+             if (!(col_name_A %in% names(db))) { stop("ERROR CHECK col_name_A TYPE=1")}
+             if (!(col_name_B %in% names(db))) { stop("ERROR CHECK col_name_B TYPE=1")}
+        } else
+        {
+            if (!(col_name[i] %in% names(db))) { stop("ERROR CHECK TYPE=0")}
+        }
+    }
+    for (i in 1:length(col_name))
+    {
+        test_cond=''
+        if (type[i]==1) {
+            col_name_A=paste0(col_name[i],'_1')
+            col_name_B=paste0(col_name[i],'_2')
+            rec_col=paste0("rec[1,'",col_name[i],"']")
+            text_cond=paste0( col_name_A, " <= ",rec_col," & ", rec_col, " <= ",  col_name_B)
+        } else
+        {
+           rec_col=paste0("rec[1,'",col_name[i],"']") 
+           text_cond=paste0( col_name[i], ' == ', rec_col)
+        }
+        print(text_cond) 
+        db <- db %>% filter(  eval(parse(text = text_cond )) )
+    }
+    return(db)
+}
+
+mail_blastula=function (subject = subject, attachmentList = NULL, addr = myaddress, 
+    body = NULL, attachFileList = NULL, adhoc = "", creds_file = "/Users/admin/Documents/dropbox/jupyter/creds/netvigator", 
+    timestamp = FALSE, from = "no.reply.blastula@netvigator.com", retry = 1) 
+{
+    require(blastula)
+    # Check if the file exists
+    if (!file.exists(creds_file)) {
+      creds_file <- "c:\\Users\\admin\\Documents\\dropbox\\jupyter\\creds\\netvigator"  
+    }
+    if (!file.exists(creds_file)) {
+      stop("creds_file problem")
+    }
+    mdbody = NULL
+    if (timestamp == TRUE) 
+        mdbody = c(mdbody, as.character(Sys.time()))
+    if (!is.null(attachmentList)) {
+        for (i in 1:length(attachmentList)) {
+            mdbody = c(mdbody, "![ABC](", attachmentList[1], 
+                ")")
+        }
+    }
+    if (!is.null(body)) {
+        if (class(body) == "list") {
+            for (i in 1:length(body)) {
+                ele = body[[i]]
+                if (inherits(ele, "formattable")) {
+                    print('formattable')
+                  mdbody <- c(mdbody, gsub('\n', '', as.htmlwidget(ele)[[1]]$html)  )
+                }
+                else if (inherits(ele, "data.frame")) {
+                  mdbody = c(mdbody, knitr::kable(ele, format = "html"))
+                }
+                else if (class(ele) == "character") {
+                  print((grepl("^(.+)/([^/]+)png$", ele)))
+                  if (grepl("^(.+)/([^/]+)png$", ele) && file.exists(ele)) {
+                    mdbody = c(mdbody, "![ABC](", ele, ")")
+                  }
+                  else {
+                    mdbody = c(mdbody, ele)
+                  }
+                }
+                else mdbody = c(mdbody, ele)
+            }
+        }
+        else {
+            mdbody = c(mdbody, body)
+        }
+    }
+    email <- compose_email(body = md(mdbody))
+    if (!is.null(attachFileList)) {
+        for (i in 1:length(attachFileList)) {
+            print(attachFileList[[i]])
+            email = email %>% add_attachment(file = attachFileList[[i]], 
+                filename = basename(attachFileList[[i]]))
+        }
+    }
+    actualtry = 0
+    callstatus = -1
+    while (callstatus == -1 && actualtry <= retry) {
+        callstatus = tryCatch({
+            smtp_send(email = email, from = from, to = addr, 
+                subject = subject, credentials = creds_file(creds_file))
+            1
+        }, error = function(cond) return(-1), warning = function(cond) return(0))
+        actualtry = actualtry + 1
+    }
+    return(actualtry)
+}
+
+mail_blastula_old=function (subject = subject, attachmentList = NULL, addr = myaddress, 
+    body = NULL, attachFileList = NULL, adhoc = "", creds_file = "/Users/admin/Documents/dropbox/jupyter/creds/netvigator", 
+    timestamp = FALSE, from = "no.reply.blastula@netvigator.com", retry = 1) 
+{
+    require(blastula)
+    # Check if the file exists
+    if (!file.exists(creds_file)) {
+      creds_file <- "c:\\Users\\admin\\Documents\\dropbox\\jupyter\\creds\\netvigator"  
+    }
+    if (!file.exists(creds_file)) {
+      stop("creds_file problem")
+    }
+    mdbody = NULL
+    if (timestamp == TRUE) 
+        mdbody = c(mdbody, as.character(Sys.time()))
+    if (!is.null(attachmentList)) {
+        for (i in 1:length(attachmentList)) {
+            mdbody = c(mdbody, "![ABC](", attachmentList[1], 
+                ")")
+        }
+    }
+    if (!is.null(body)) {
+        if (class(body) == "list") {
+            for (i in 1:length(body)) {
+                ele = body[[i]]
+                if (inherits(ele, "data.frame")) {
+                  mdbody = c(mdbody, knitr::kable(ele, format = "html"))
+                }
+                else if (class(ele) == "character") {
+                  print((grepl("^(.+)/([^/]+)png$", ele)))
+                  if (grepl("^(.+)/([^/]+)png$", ele) && file.exists(ele)) {
+                    mdbody = c(mdbody, "![ABC](", ele, ")")
+                  }
+                  else {
+                    mdbody = c(mdbody, ele)
+                  }
+                }
+                else mdbody = c(mdbody, ele)
+            }
+        }
+        else {
+            mdbody = c(mdbody, body)
+        }
+    }
+    email <- compose_email(body = md(mdbody))
+    if (!is.null(attachFileList)) {
+        for (i in 1:length(attachFileList)) {
+            print(attachFileList[[i]])
+            email = email %>% add_attachment(file = attachFileList[[i]], 
+                filename = basename(attachFileList[[i]]))
+        }
+    }
+    actualtry = 0
+    callstatus = -1
+    while (callstatus == -1 && actualtry <= retry) {
+        callstatus = tryCatch({
+            smtp_send(email = email, from = from, to = addr, 
+                subject = subject, credentials = creds_file(creds_file))
+            1
+        }, error = function(cond) return(-1), warning = function(cond) return(0))
+        actualtry = actualtry + 1
+    }
+    return(actualtry)
+}
---
+++ message_lab.r
@@ -0,0 +1,126 @@
+
+getEnglishMsg=function(res, message='')
+{
+    upcnt=0
+    downcnt=0
+    if  (any (as.vector( res[ grepl(">=", res$Prediction), 'Odd']<0.25) ))
+    {
+        message=paste0(message,"\nthere is a low chance it will go up. \n")
+        downcnt=downcnt+1
+    }
+    if  (any (as.vector(res[grepl(">=", res$Prediction), 'Odd']>0.75) ))
+    {
+        message=paste0(message,"\nThere is a high chance it will go up.\n")
+        upcnt=upcnt+1
+    }
+    if  (any (as.vector(res[grepl("<=", res$Prediction), 'Odd']<0.25) ))
+    {
+        message=paste0(message,"\nthere is a low chance it will go down. \n")
+        upcnt=upcnt+1
+    }
+    if  (any (as.vector(res[grepl("<=", res$Prediction), 'Odd']>0.75) ))
+    {
+        message=paste0(message,"\nThere is a high chance it will go down. \n")
+        downcnt=downcnt+1
+    }
+   if (downcnt==0 && upcnt==0) message='There is no clear or useful forecast.'
+     if (downcnt>0 && upcnt>0) message='The price may fluctuate within a narrow range'
+    print(message)
+    return(message)
+}
+
+getChineseMsg=function(res, messageV='', type='message', lowerV=0.25, upperV=0.75)
+{
+    upcnt=0
+    downcnt=0
+    pattern=NULL
+
+    M1TIME="[TIME]"
+    if (any(grepl("pv.f1", res$Prediction))) M1TIME="今天"
+    if (any(grepl("wkhwk_pv.f1", res$Prediction))) M1TIME="本週和上週"
+    
+	if  (any (as.vector( res[ grepl(">=", res$Prediction), 'Odd']<lowerV) ))
+	{ 
+        M1="\n上漲高於#TIME#最高價的機率為低. \n"
+        M1=gsub('#TIME#', M1TIME, M1)
+		messageV=paste0(messageV,M1)
+        downcnt=downcnt+1
+        pattern=c(pattern,'not 2')
+	}
+
+	if  (any (as.vector( res[ grepl(">=", res$Prediction), 'Odd'] >upperV) ))
+	{
+		M1="\n上漲高於#TIME#最高價的機率為高. \n"
+        M1=gsub('#TIME#', M1TIME, M1)
+		messageV=paste0(messageV,M1)
+        upcnt=upcnt+1
+        pattern=c(pattern,'2')
+	}
+     
+	if  (any (as.vector( res[ grepl("<=", res$Prediction), 'Odd']<lowerV) ))
+	{
+		M1="\n下跌低於#TIME#最低價的機率為低. \n"
+        M1=gsub('#TIME#', M1TIME, M1)
+		messageV=paste0(messageV,M1)
+        upcnt=upcnt+1
+        pattern=c(pattern,'not -2')
+	}
+	if  (any (as.vector(res[ grepl("<=", res$Prediction) , 'Odd']>upperV) ))
+	{
+		M1="\n下跌低於#TIME#最低價的機率為高. \n"
+        M1=gsub('#TIME#', M1TIME, M1)
+		messageV=paste0(messageV,M1)
+        downcnt=downcnt+1
+        pattern=c(pattern,'-2')
+	}
+	if (downcnt==0 && upcnt==0) messageV='目前尚無明確或有用的預測。'
+    if (downcnt>0 && upcnt>0) messageV='價格可能在不大範圍內波動。'
+   
+    expected=0 #Used for backtest
+
+    if (downcnt==0 && upcnt==0) expected=0
+    if (downcnt>0 && upcnt>0) expected=0
+    if (length(pattern)==1) expected=pattern
+    if (length(pattern)>1 && downcnt==0) expected="2"
+    if (length(pattern)>1 && upcnt==0) expected="-2"    
+    if (type=='pattern') return (list(pattern=pattern,expected=expected))
+    
+	if (type!='pattern') print(messageV)
+	return(messageV)
+}
+
+standardName=function(db, shrink=1)
+{   #c( old -> new)
+    namelist=list(c('targetcond','Prediction'), 
+                  c('Pb','Odd'), c('hit', 'CN'), c('total','tot'), c('odd', 'Odd')
+                  )
+    for (i in 1:length(namelist))
+    {
+        oldname=namelist[[i]][1]
+        newname=namelist[[i]][2]
+      if (oldname %in% names(db)) 
+                db <- db %>% rename(`:=`(!!newname, all_of(oldname)))
+    }
+    if (shrink==1) db=db[, c('Prediction','Odd','CN','Tot')]
+    rownames(db)=NULL
+    return(db)
+}
+
+getMethodText=function() 
+{
+  return("方法")
+}
+getPredictionText=function() 
+{
+ return("預測")
+}
+getPastPerfText=function() 
+{
+    return("過往預測表現")
+}
+
+getChineseTitle=function()
+{
+    msg='自家研發2.0'
+	return(msg)
+}
