5a4e07e63296c29f2baeb4fbc23c101aa166c135
kimmanlui <cskmlui@hotmail.com>
2026-03-18 11:07:32 +0800

    Update message_lab.r

--- message_lab.r
+++ message_lab.r
@@ -1,7 +1,7 @@
 
 
 
-getChineseMsg=function(res, messageV='', type='message', lowerV=0.25, upperV=0.75)
+getChineseMsg_OLD=function(res, messageV='', type='message', lowerV=0.25, upperV=0.75)
 {
     upcnt=0
     downcnt=0
@@ -60,6 +60,87 @@ getChineseMsg=function(res, messageV='', type='message', lowerV=0.25, upperV=0.7
 	if (type!='pattern') print(messageV)
 	return(messageV)
 }
+
+
+getChineseMsg=function(res, messageV='', type='message', lowerV=0.25, upperV=0.75)
+{
+    upcnt=0
+    downcnt=0
+    pattern=NULL
+
+    M1TIME="[TIME]"
+    if (any(grepl("pv.lg2.f2", res$Prediction))) 
+    {
+        M1TIME="今天"
+        FUTURE="下兩個交易日"
+    }
+    if (any(grepl("pv.f1", res$Prediction))) 
+    {
+        M1TIME="今天"
+        FUTURE="下交易日"
+    }
+    if (any(grepl("wkhwk_pv.f1", res$Prediction))) 
+    {
+        M1TIME="本週和上週"
+        FUTURE="下交易日"
+    }
+    
+	if  (any (as.vector( res[ grepl(">=", res$Prediction), 'Odd']<lowerV) ))
+	{ 
+        M1="\n#FUTURE#上漲高於#TIME#最高價的機率為低. \n"
+        M1=gsub('#TIME#', M1TIME, M1)
+        M1=gsub('#FUTURE#', FUTURE, M1)
+		messageV=paste0(messageV,M1)
+        downcnt=downcnt+1
+        pattern=c(pattern,'not 2')
+	}
+
+	if  (any (as.vector( res[ grepl(">=", res$Prediction), 'Odd'] >upperV) ))
+	{
+		M1="\n#FUTURE#上漲高於#TIME#最高價的機率為高. \n"
+        M1=gsub('#TIME#', M1TIME, M1)
+        M1=gsub('#FUTURE#', FUTURE, M1)
+		messageV=paste0(messageV,M1)
+        upcnt=upcnt+1
+        pattern=c(pattern,'2')
+	}
+     
+	if  (any (as.vector( res[ grepl("<=", res$Prediction), 'Odd']<lowerV) ))
+	{
+		M1="\n#FUTURE#下跌低於#TIME#最低價的機率為低. \n"
+        M1=gsub('#TIME#', M1TIME, M1)
+        M1=gsub('#FUTURE#', FUTURE, M1)
+		messageV=paste0(messageV,M1)
+        upcnt=upcnt+1
+        pattern=c(pattern,'not -2')
+	}
+	if  (any (as.vector(res[ grepl("<=", res$Prediction) , 'Odd']>upperV) ))
+	{
+		M1="\n#FUTURE#下跌低於#TIME#最低價的機率為高. \n"
+        M1=gsub('#TIME#', M1TIME, M1)
+        M1=gsub('#FUTURE#', FUTURE, M1)
+		messageV=paste0(messageV,M1)
+        downcnt=downcnt+1
+        pattern=c(pattern,'-2')
+	}
+	if (downcnt==0 && upcnt==0) messageV='目前尚無明確或有用的預測。'
+    if (downcnt>0 && upcnt>0) messageV='價格可能在不大範圍內波動。'
+   
+    expected=0 #Used for backtest
+
+    if (downcnt==0 && upcnt==0) expected=0
+    if (downcnt>0 && upcnt>0) expected=0
+    if (length(pattern)==1) expected=pattern
+    if (length(pattern)>1 && downcnt==0) expected="2"
+    if (length(pattern)>1 && upcnt==0) expected="-2"    
+    if (type=='pattern') return (list(pattern=pattern,expected=expected))
+    
+	if (type!='pattern') print(messageV)
+	return(messageV)
+}
+
+
+
 
 standardName=function (db, shrink = 1) 
 {
